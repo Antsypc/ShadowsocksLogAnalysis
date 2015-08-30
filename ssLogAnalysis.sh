7db351364bb30ee2f0feedac5b35be9546439dc7
YPC <949688912@qq.com>
2015-08-30 18:04:14 +0800

    add the location of IP address.

--- ssLogAnalysis.sh
+++ ssLogAnalysis.sh
@@ -8,6 +8,17 @@ then
 	exit
 fi
 
-awk '/connecting .*:[0-9]+ from .*:[0-9]+/{split($7,IP,":");USER[IP[1]]++;} END{print "user","        login times"; for(n in USER)print n,USER[n];}' shadowsocks.log
+ips=(`awk '/connecting .*:[0-9]+ from .*:[0-9]+/{split($7,IP,":");USER[IP[1]]++;} END{for(n in USER)print n,USER[n];}' shadowsocks.log`)
+i=0
+for ip in ${ips[@]}
+do
+	i=`expr $i + 1 `
+	if [ `expr $i % 2` != 0 ]
+	then
+		echo -ne $ip"\t"
+		curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]+' 'BEGIN{ORS=" "} {location=substr($5,19);printf("%-50s",location)}'
+	else
+		echo -e ${ip}
+	fi
+done
 
-exit
