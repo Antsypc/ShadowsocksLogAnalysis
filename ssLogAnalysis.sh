4e8530c8001c98d5ad60f62f712d933163d23c06
YPC <949688912@qq.com>
2015-09-01 16:27:18 +0800

    got the reason of different awk

--- ssLogAnalysis.sh
+++ ssLogAnalysis.sh
@@ -10,17 +10,23 @@ then
 fi
 
 ips=(`awk '/connecting .*:[0-9]+ from .*:[0-9]+/{split($7,IP,":");USER[IP[1]]++;} END{for(n in USER)print n,USER[n];}' /var/log/shadowsocks.log`)
-echo -e "IP\t\tlocation\t\t\t\t\t\tlogin times"
+echo -e "IP\t\tlocation\t\t\t\tlogin times"
 for ip in ${ips[@]}
 do
 	((i++))
 	if [ $((i % 2)) != 0 ]
 	then
 		echo -ne $ip"\t"
-		# ubuntu14.04
-		#curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]+' 'BEGIN{ORS=" "} {print $0;location=substr($5,19);printf("%-50s",location)}'
-		# centOS6
-		curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,7);printf("%-50s",location);}'
+		# Because of the differences of awk between ubuntu and centOS,there are some different results.
+		# As follow,the awk version of Debian is mawk but centOS is gawk.
+		# In my test OS,the substr(a,b,c) in awk,a is the character location in ubuntu while a is the word location in centOS.
+		# gawk is much more popular,while mawk maybe more efficient.
+		# So,choose different command as follow.
+
+		# mawk 1.2
+		curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,19);printf("%-50s",location)}'
+		# gawk 3.1.7
+		#curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,7);printf("%-50s",location);}'
 	else
 		echo -e ${ip}
 	fi
