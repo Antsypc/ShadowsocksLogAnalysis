642844fa62bfe5a6ec9a0e9fccff217583a52bca
YPC <949688912@qq.com>
2015-09-07 20:40:02 +0800

    sorted by descending order and then echo to terminal

--- ssLogAnalysis.sh
+++ ssLogAnalysis.sh
@@ -9,26 +9,42 @@ then
 	exit
 fi
 
-ips=(`awk '/connecting .*:[0-9]+ from .*:[0-9]+/{split($7,IP,":");USER[IP[1]]++;} END{for(n in USER)print n,USER[n];}' /var/log/shadowsocks.log`)
-echo -e "IP\t\tlocation\t\t\t\tlogin times"
+#array ips is consists of IP,latest login time,login times,
+#and sorted by login times in descending order
+ips=$(awk '  
+	/connecting .*:[0-9]+ from .*:[0-9]+/{ 
+		split($7,IP,":");
+		USER[IP[1]]++;
+		LatestLogin[IP[1]]=$1"/"$2;
+	} 
+	END{ 
+		for(n in USER) 
+			print n,LatestLogin[n],USER[n] | "sort -r -n -k3";
+	}' /var/log/shadowsocks.log)
+
+echo -e "IP\t\tlocation\t\t\t\tlatest login\t\tlogin times"
 for ip in ${ips[@]}
 do
 	((i++))
-	if [ $((i % 2)) != 0 ]
+	if [ $((i % 3)) == 0 ]
 	then
-		echo -ne $ip"\t"
+		echo -e "\t${ip}"	#print login times
+	elif [ $((i % 3)) == 1 ]
+	then
+		echo -ne $ip"\t"	#print IP
 		# Because of the differences of awk between ubuntu and centOS,there are some different results.
 		# As follow,the awk version of Debian is mawk but centOS is gawk.
 		# In my test OS,the substr(a,b,c) in awk,a is the character location in ubuntu while a is the word location in centOS.
 		# gawk is much more popular,while mawk maybe more efficient.
 		# So,choose different command as follow.
 
-		# mawk 1.2
-		curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,19);printf("%-50s",location)}'
-		# gawk 3.1.7
+		# mawk 1.2,print location
+		curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,19);printf("%-50s",location);}'
+		# gawk 3.1.7,print location
 		#curl -s "http://ip138.com/ips138.asp?ip=${ip}&action=2" | iconv -f gb2312 -t utf-8 | grep '<ul class="ul1"><li>' | awk -F'[><]' 'BEGIN{ORS=" "} {location=substr($7,7);printf("%-50s",location);}'
-	else
-		echo -e ${ip}
+	elif [ $((i % 3)) == 2 ]
+	then
+		echo -n ${ip}	#print latest login
 	fi
 done
 
